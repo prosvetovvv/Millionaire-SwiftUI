522437dbe302d8e86afe6b608d3ed571f7128bb3
prosvetovvv <vp12@yandex.ru>
2020-12-11 10:50:12 +0300

    Initial Commit

---
+++ Millionaire-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Millionaire-SwiftUI
+//
+//  Created by Vitaly Prosvetov on 11.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Millionaire-SwiftUI/Millionaire_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Millionaire_SwiftUIApp.swift
+//  Millionaire-SwiftUI
+//
+//  Created by Vitaly Prosvetov on 11.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Millionaire_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
